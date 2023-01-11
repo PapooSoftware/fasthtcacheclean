e0ce3da3b997b4049acb34cb1344e4de2b702042
Christoph Grenz <cg@papoo.de>
2023-01-11 18:53:31 +0100

    Copy man page file to build directory on build

--- build.rs
+++ build.rs
@@ -1,6 +1,7 @@
 // Copyright (c) 2022 Christoph Grenz (Papoo Software & Media GmbH) <info@papoo.de>
 // SPDX-License-Identifier: LGPL-3.0-or-later
 use clap::CommandFactory;
+use copy_to_output::copy_to_output;
 
 #[path = "src/cmdargs.rs"]
 mod cmdargs;
@@ -9,14 +10,16 @@ mod cmdargs;
 mod size_spec;
 
 fn main() -> std::io::Result<()> {
-	let out_dir = std::path::PathBuf::from(std::env::var_os("OUT_DIR").ok_or_else(|| std::io::ErrorKind::NotFound)?);
+	let out_dir = std::path::PathBuf::from(std::env::var_os("OUT_DIR").ok_or(std::io::ErrorKind::NotFound)?);
+	let out_file = out_dir.join("fasthtcacheclean.8");
 
 	let cmd = cmdargs::Args::command();
 	let man = clap_mangen::Man::new(cmd);
 	let mut buffer: Vec<u8> = Default::default();
 	man.render(&mut buffer)?;
 
-	std::fs::write(out_dir.join("fasthtcacheclean.8"), buffer)?;
+	std::fs::write(&out_file, buffer)?;
+	copy_to_output(out_file.to_str().ok_or(std::io::ErrorKind::InvalidData)?, &std::env::var("PROFILE").unwrap()).expect("Could not copy");
 
 	Ok(())
 }
